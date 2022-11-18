3fbe866bacbe37d324243e9a0c2c3937f3dbec6d
sjtuljw520 <sjtuljw520@gmail.com>
2022-11-18 13:49:58 +0800

    add testing on waymo test set

--- waymo_run_test.sh
+++ waymo_run_test.sh
@@ -1,38 +1,34 @@
 det_name=$1
 waymo_data_dir=$2
 waymo_result_dir=$3
-gt_dets_dir=$4
-proc_num=$5
+proc_num=$4
 
 # for vehicle
-python tools/main_waymo.py \
+python tools/main_waymo_test.py \
     --name st_pvrcnnpp \
     --det_name ${det_name} \
     --obj_type vehicle \
     --config_path configs/waymo_configs/vc_kf_giou_nopc.yaml \
     --data_folder ${waymo_data_dir} \
     --result_folder ${waymo_result_dir} \
-    --gt_folder ${gt_dets_dir} \
     --process ${proc_num}
 
 # for pedestrian
-python tools/main_waymo.py \
+python tools/main_waymo_test.py \
     --name st_pvrcnnpp \
     --det_name ${det_name} \
     --obj_type pedestrian \
     --config_path configs/waymo_configs/pd_kf_giou_nopc.yaml \
     --data_folder ${waymo_data_dir} \
     --result_folder ${waymo_result_dir} \
-    --gt_folder ${gt_dets_dir} \
     --process ${proc_num}
 
 # for cyclist
-python tools/main_waymo.py \
+python tools/main_waymo_test.py \
     --name st_pvrcnnpp \
     --det_name ${det_name} \
     --obj_type cyclist \
     --config_path configs/waymo_configs/cc_kf_giou_nopc.yaml \
     --data_folder ${waymo_data_dir} \
     --result_folder ${waymo_result_dir} \
-    --gt_folder ${gt_dets_dir} \
     --process ${proc_num}
